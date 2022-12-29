de751c1ede09b3dfa63a7098dc17a5f64d7531aa
gdarkwah <61522557+gdarkwah@users.noreply.github.com>
2022-12-29 09:55:00 -0800

    Load session lifetime from env variables

--- sessionStore.js
+++ sessionStore.js
@@ -4,26 +4,19 @@ var session = require("express-session");
 var MySQLStore = require("express-mysql-session")(session);
 // var crypto = require("crypto");
 
-const DB_HOST = process.env.DB_HOST;
-const DB_USER = process.env.DB_USER;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_DATABASE = process.env.DB_DATABASE;
-const DB_PORT = process.env.DB_PORT;
-
 options = {
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD, // use dotenv to load the hidden environmental variable. Look further
-  database: DB_DATABASE, // Database name
-  port: DB_PORT, // port name
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD, // use dotenv to load the hidden environmental variable. Look further
+  database: process.env.DB_DATABASE, // Database name
+  port: process.env.DB_PORT, // port name
 };
 
 const connection = mysql.createPool(options);
 var sessionStore = new MySQLStore(
   {
     createDatabaseTable: false,
-    expiration: 86400000,
-    // expiration: 300000,
+    expiration: process.env.SESS_LIFETIME,
     schema: {
       tableName: "sessions",
       columnNames: {
