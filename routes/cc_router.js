cc7dfa706ab3eb96b21e2756cafc0bde728cf319
gdarkwah <61522557+gdarkwah@users.noreply.github.com>
2022-12-29 12:57:45 -0800

    add "/cropclassifier/startup" route

--- routes/cc_router.js
+++ routes/cc_router.js
@@ -1,15 +1,18 @@
 var express = require("express");
 var cc = require("../crop_classifier");
 
-// special functions for the main cropclassification app
-
-
 var router = express.Router();
 
-
 /* GET the crop classifier home page. */
-router.get('/cropclassifier', function(req, res, next) {
-    res.render('crop_classifier/index', { title: 'Crop Classifier - Bangladesh' });
+router.get("/cropclassifier", function (req, res, next) {
+  res.render("crop_classifier/index", {
+    title: "Crop Classifier - Bangladesh",
   });
+});
+
+router.get(
+  "/cropclassifier/startup",
+  cc.startup // invoke startup function
+);
 
 module.exports = router;
