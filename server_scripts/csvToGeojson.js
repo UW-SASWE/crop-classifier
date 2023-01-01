b4f3722c3659789711521e58f04598dab6d56a4e
gdarkwah <61522557+gdarkwah@users.noreply.github.com>
2023-01-01 09:10:28 -0800

    new csvParser

--- server_scripts/csvToGeojson.js
+++ server_scripts/csvToGeojson.js
@@ -1,28 +1,32 @@
 const fs = require("fs");
-const csvParser = require("csv-parser");
-
-// var path = "./data.csv";
-
-// const result = [];
-// var geoJSON = { type: "FeatureCollection", features: [] };
+const { parse } = require("csv-parse");
 
 const csvToGeojson = (
-  path,
-  result = [],
+  csvInput,
+  records = [],
   geoJSON = { type: "FeatureCollection", features: [] }
 ) =>
   new Promise((resolve, reject) => {
-    fs.createReadStream(path)
-      .pipe(csvParser())
-      .on("data", (data) => {
-        result.push(data);
+    parse(csvInput, {
+      trim: true,
+      columns: true,
+      skip_empty_lines: true,
+    })
+      .on("readable", function () {
+        let record;
+        while ((record = this.read()) !== null) {
+          records.push(record);
+        }
       })
       .on("end", () => {
-        for (let i = 0, len = result.length; i < len; i++) {
-          var pointData = result[i];
+        for (let i = 0, len = records.length; i < len; i++) {
+          var pointData = records[i];
           geoJSON.features.push({
             geometry: {
-              coordinates: [Number(pointData.Latitude), Number(pointData.Longitude)],
+              coordinates: [
+                Number(pointData.Latitude),
+                Number(pointData.Longitude),
+              ],
               type: "Point",
             },
             id: i,
@@ -38,12 +42,36 @@ const csvToGeojson = (
   });
 
 // // example usage of the csvToGeojson function
+// var a = `
+// Latitude,Longitude,classCode,class
+// 88.97475,24.10169444,1,rice
+// 88.97511111,24.10736111,1,rice
+// 88.96127778,24.09975,1,rice
+// 88.97116667,24.09441667,1,rice
+// 88.97116667,24.09441667,1,rice
+// 88.95675,24.09191667,1,rice
+// 88.96488889,24.1005,1,rice
+// 88.96152778,24.09586111,1,rice
+// 88.00277778,24.08097222,1,rice
+// 88.99722222,24.08597222,1,rice
+// 88.97475,24.04483333,1,rice
+// 88.97005556,24.03694444,1,rice
+// 88.98263889,24.01386111,1,rice
+// 89.01161111,24.01480556,1,rice
+// 89.01211111,24.03505556,1,rice
+// 89.01211111,24.03502778,1,rice
+// 88.99911111,24.03925,1,rice
+// 88.99477778,24.03011111,1,rice
+// 89.01188889,24.01916667,1,rice
+// 88.98913889,24.024,1,rice
+// `;
+//
 // (async function () {
 //   try {
-//     var geoJSON = await csvToGeojson("./cc_assets/training_points.csv");
+//     var geoJSON = await csvToGeojson(a);
 //     // console.log(geoJSON);
 //     fs.writeFile(
-//       "./cc_assets/training_points.geojson",
+//       "./sandbox.geojson",
 //       JSON.stringify(geoJSON),
 //       (err) => {
 //         if (err) {
