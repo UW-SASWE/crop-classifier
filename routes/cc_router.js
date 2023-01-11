6827c8d327ea1d8a64196bd893f3b50254126ec7
gdarkwah <61522557+gdarkwah@users.noreply.github.com>
2023-01-10 16:13:59 -0800

    consolidated load polygon

--- routes/cc_router.js
+++ routes/cc_router.js
@@ -13,21 +13,7 @@ router.get("/cropclassifier", function (req, res, next) {
 
 router.get("/cropclassifier/startup", cc.startup);
 
-// Get and display the basic polygons on the map
-// router.get("/cropclassifier/bg_boundary", cc.loadCountry); // get entire country
-// router.get("/cropclassifier/bg_divisions", cc.loadDivisions); // get entire divisions
-// router.get("/cropclassifier/bg_zilas", cc.loadZilas); // get entire zilas
-// router.get("/cropclassifier/bg_upazilas", cc.loadUpazilas); // get entire upazilas
-// router.get("/cropclassifier/bg_unions", cc.loadUnions); // get entire unions
 router.get("/cropclassifier/loadPolygon", cc.loadPolygon); // request for a polygon
-
-// Post request to display specific boundaries (filtered)
-// router.post("/cropclassifier/bg_boundary", cc.loadCountry); // request for a country
-// router.post("/cropclassifier/bg_divisions", cc.loadDivisions); // request for a division
-// router.post("/cropclassifier/bg_zilas", cc.loadZilas); // request for a zila
-// router.post("/cropclassifier/bg_upazilas", cc.loadUpazilas); // request for an upazila
-// router.post("/cropclassifier/bg_unions", cc.loadUnions); // request for a union
-
 router.post("/cropclassifier/loadPolygon", cc.loadPolygon); // request for a polygon
 
 
